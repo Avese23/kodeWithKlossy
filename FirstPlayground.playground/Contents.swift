81d2e69d33c518f337f2f79577c9990d6a15b744
Apple <apple@Apples-MacBook-Pro-107.local>
2019-07-22 13:57:09 -0400

    my first playground

---
+++ FirstPlayground.playground/Contents.swift
@@ -0,0 +1,83 @@
+import UIKit
+
+var str = "Hello, playground"
+str
+
+var name = "avery"
+name
+var pet = "catacorn"
+pet
+str
+name = "Avese"
+name
+print("hello world")
+var age = "14"
+print (age)
+print ("I am \(age)")
+print ("call me \(name)")
+name = "avery"
+var first = "avery"
+var last = "selk"
+print ("I am \(first)")
+print (last)
+print ("I am \(first)\(last)")
+print ("I am \(first) \(last)")
+var a = 12.0
+var b = 65.0
+var c = 31.0
+var d = 98.0
+print ((a+b+c+d)/4)
+5<7
+12>7
+6 != 8
+"avery" == "avery"
+"avery" == " avery"
+"Avery" == "avery"
+var favNum = 23
+favNum
+var dogAge = 12
+if dogAge < 2 {
+    print ("You are a puppy")
+}
+else if dogAge > 12 {
+    print ("You are elderly")
+}
+else {
+    print ("You are totally awesome")
+}
+var favoriteFood = "Peppermint Patties"
+if favoriteFood == "M&M pancakes" {
+    print ("make breakfast yourself")
+}
+else if favoriteFood == "Coq au vin" {
+    print ("ask for help with your dinner")
+}
+else {
+    print ("INTRUDER!!")
+}
+favoriteFood = "M&M pancakes"
+if favoriteFood == "M&M pancakes" {
+    print ("make breakfast yourself")
+}
+else if favoriteFood == "Coq au vin" {
+    print ("ask for help with your dinner")
+}
+else {
+    print ("INTRUDER!!")
+}
+favoriteFood = "Coq au vin"
+if favoriteFood == "M&M pancakes" {
+    print ("make breakfast yourself")
+}
+else if favoriteFood == "Coq au vin" {
+    print ("ask for help with your dinner")
+}
+else {
+    print ("INTRUDER!!")
+}
+if pet == "Doggie" {
+    print ("Cuuute dog")
+}
+else {
+    print ("am I dreaming?")
+}
